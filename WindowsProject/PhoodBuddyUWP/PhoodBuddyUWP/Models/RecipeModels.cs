6083679bb8a35dc0baa4a3a24a4984b9057e29a7
Chuck Flowers <Chuck Flowers>
2016-03-17 16:17:51 -0400

    Adds first couple of XAML pages for the windows project

---
+++ WindowsProject/PhoodBuddyUWP/PhoodBuddyUWP/Models/RecipeModels.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace PhoodBuddyUWP.Models
+{
+    class RecipeIngredient
+    {
+        //Properties
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public float Amount { get; set; }
+
+        //Constructor
+        public RecipeIngredient(string n, string u, int a)
+        {
+            Name = n;
+            Unit = u;
+            Amount = a;
+        }
+
+        //ToString Override
+        public override string ToString()
+        {
+            if (Amount == 1)
+                return Amount + " " + Unit + " of " + Name;
+            else
+                return Amount + " " + Unit + "s of" + Name;
+        }
+    }
+
+    class RecipeInstruction
+    {
+        //Properties
+        public int StepNumber { get; set; }
+        public string Instruction { get; set; }
+
+        //Constructor
+        public RecipeInstruction(int n, string i)
+        {
+            StepNumber = n;
+            Instruction = i;
+        }
+
+        //ToString Override
+        public override string ToString()
+        {
+            return StepNumber + ".) " + Instruction;
+        }
+    }
+
+    class Recipe
+    {
+        //Properties
+        public string Title { get; set; }
+        public List<RecipeIngredient> Ingredients { get; set; }
+        public List<RecipeInstruction> Instructions { get; set; }
+    }
+}
