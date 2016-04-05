ad2250ba9c4435cfa3e99b71d534e61283c01d20
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-04-04 20:06:33 -0400

    Adds functionality to allow users to register or login through 3rd party providers. Adds Custom Email/Password Registration and Login. BUG: Validating emails

--- js/auth.js
+++ js/auth.js
@@ -4,6 +4,39 @@
 
 //This function is used as a input for auth functions that manages return of either error or authData
 //To be used with: fblogin, twitterlogin, googlelogin.
+function authLogin(error, authData)
+{
+	if (error)
+	{
+		console.log("Login Failed!", error)
+	}
+	else
+	{
+		console.log("Authenticated successfully with payload:", authData);
+
+		var usersRef = new Firebase("https://phoodbuddy.firebaseio.com/users");
+  		usersRef.once('value', function(snapshot){
+	  	if(snapshot.hasChild(authData.uid))
+	  	{
+	  		console.log("User stays logged in for having an account");
+	  	}
+	  	else
+	  	{
+	  		console.log("USER MUST CREATE ACCOUNT")
+	  		var ref = new Firebase("https://phoodbuddy.firebaseio.com");
+	  		ref.unauth();
+	  	}
+		
+		})
+	}
+}
+
+function authLogout()
+{
+	var ref = new Firebase("https://phoodbuddy.firebaseio.com");
+	ref.unauth();
+}
+
 function authHandler(error, authData) 
 {
 
@@ -21,7 +54,56 @@ function authHandler(error, authData)
   }	
 }
 
-function fblogin()
+// Create a callback which logs the current auth state
+function authDataCallback(authData) {
+  if (authData) {
+    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+  } else {
+    console.log("User is logged out");
+  }
+}
+
+// Register the callback to be fired every time auth state changes
+//var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+//ref.onAuth(authDataCallback);
+
+
+function customRegister(emailString, passwordString)
+{
+	var ref = new Firebase("https://phoodbuddy.firebaseio.com");
+	ref.createUser({
+  		email    : emailString,
+  		password : passwordString
+	}, function(error, userData) {
+  		if (error) {
+   	 		console.log("Error creating user:", error);
+  		} 
+  		else 
+  		{
+    		console.log("Successfully created user account with uid:", userData);
+  		}
+	});
+}
+
+function customLogin(emailString, passwordString)
+{
+	var ref = new Firebase("https://phoodbuddy.firebaseio.com");
+	ref.authWithPassword({
+  		email    : emailString,
+  		password : passwordString
+	}, function(error, authData) {
+  		if (error) {
+    		console.log("Login Failed!", error);
+  		} 
+  		else 
+  		{
+    		console.log("Authenticated successfully with payload:", authData);
+  		}
+	});
+}
+
+
+function fbRegister()
 {
 	// Simple login for facebook. authData has user data
 
@@ -30,7 +112,13 @@ function fblogin()
 	ref.authWithOAuthPopup("facebook", authHandler);
 }
 
-function twitterlogin() 
+function fbLogin()
+{
+	var ref = new Firebase("https://phoodbuddy.firebaseio.com");
+	ref.authWithOAuthPopup("facebook", authLogin);
+}
+
+function twitterRegister() 
 {
 	//Simple login for twitter. authData has user data
 
@@ -39,15 +127,26 @@ function twitterlogin()
 	ref.authWithOAuthPopup("twitter", authHandler);
 }
 
+function twitterLogin()
+{
+	var ref = new Firebase("https://phoodbuddy.firebaseio.com");
+	ref.authWithOAuthPopup("twitter", authLogin);
+}
 
 
-function googlelogin(){
+function googleRegister(){
 
 	//Simple login for twitter. authData has user data
 	//This creates reference to database that than uses that reference call to authenticate with current user.
 
 	var ref = new Firebase("https://phoodbuddy.firebaseio.com");
 	ref.authWithOAuthPopup("google", authHandler);
+}
+
+function googleLogin()
+{
+	var ref = new Firebase("https://phoodbuddy.firebaseio.com");
+	ref.authWithOAuthPopup("google", authLogin);
 }
 
 
