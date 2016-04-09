ea5b958877aff28701834dddb74529ab07211c59
William Funk <contact@WilliamRADFunk.com>
2016-04-09 16:25:46 -0400

    Installed the bootstrap modal in each of the existing pages. Connected it to the grocery page's 'printgrocery list' link/button to show that the styling works. This modal template will be used for all front-end communication with the user.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@ $(document).ready(function ()
 {
 	$('.carousel').carousel({
 	    interval: false
-	}); 
+	});
+	$("#printList").click(function(){
+		$(".modal-header").html("Print Grocery List");
+		$(".modal-body").html("<p>Are you sure you want to print your grocery list?</p>");
+        $("#modal").modal({backdrop: "static"});
+    });
 });
 $(window).resize(function()
 {
