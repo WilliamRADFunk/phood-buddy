33593cf4460a269d76fae077e5b169e03db5699e
William Funk <contact@WilliamRADFunk.com>
2016-04-09 17:30:14 -0400

    Small stying changes. Further adjustment to the modal.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 $(document).ready(function ()
 {
+	// Controls how long between carousel transitions.
 	$('.carousel').carousel({
-	    interval: false
+	    interval: 2000
 	});
-	$("#printList").click(function(){
+	// Specifies content and functionality of modal when
+	// user clicks specific links/buttons.
+	$("#print-list").click(function(){
+		$("#modal").modal({backdrop: "static", keyboard: false, show: true});
 		$(".modal-header").html("Print Grocery List");
 		$(".modal-body").html("<p>Are you sure you want to print your grocery list?</p>");
-        $("#modal").modal({backdrop: "static"});
+    });
+    $("#lost-password").click(function(){
+    	$("#modal").modal({backdrop: "static", keyboard: false, show: true});
+		$(".modal-header").html("Reset Password");
+		$(".modal-body").html("<form><label>Registered Email: </label><input type='text' /></form>");
     });
 });
 $(window).resize(function()
