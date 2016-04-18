0005fe5fcc289d248a669b019ff1c5b3ea7bdb1e
Chuck Flowers <Chuck Flowers>
2016-04-18 04:54:04 -0400

    UserModel implements INotifyPropertyChange to facilitate faster and more correct UI updates when data is changed. Adds MyRecipesPage to be implemented in future commit

--- WindowsProject/PhoodBuddyUWP/PhoodBuddyUWP/Models/UserModel.cs
+++ WindowsProject/PhoodBuddyUWP/PhoodBuddyUWP/Models/UserModel.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Storage;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace PhoodBuddyUWP.Models
 {
@@ -25,7 +22,7 @@ namespace PhoodBuddyUWP.Models
         VEGETARIAN
     }
 
-    class UserModel
+    public class UserModel : INotifyPropertyChanged
     {
         //CONSTRUCTOR
         public UserModel()
@@ -34,18 +31,86 @@ namespace PhoodBuddyUWP.Models
             UserPass = "";
             UserFirstName = "";
             UserLastName = "";
-            UserAllergies = new List<int>();
-            UserHealthFactors = new List<int>();
-            UserTastes = new List<float>();
+            UserAllergies = new ObservableCollection<int>();
+            UserHealthFactors = new ObservableCollection<int>();
+            UserTastes = new ObservableCollection<float>();
         }
 
+
         //PUBLIC PROPERTIES
-        public string UserEmail { get; set; }
-        public string UserPass { get; set; }
-        public string UserFirstName { get; set; }
-        public string UserLastName { get; set; }
-        public List<int> UserAllergies { get; set; }
-        public List<int> UserHealthFactors { get; set; }
-        public List<float> UserTastes { get; set; }
+        public string UserEmail
+        {
+            get
+            {
+                return _userEmail;
+            }
+            set
+            {
+                _userEmail = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UserEmail)));
+            }
+        } private string _userEmail;
+
+        public string UserPass
+        {
+            get
+            {
+                return _userPass;
+            }
+            set
+            {
+                _userPass = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UserPass)));
+            }
+        } private string _userPass;
+
+        public string UserFirstName
+        {
+            get
+            {
+                return _userFirstName;
+            }
+            set
+            {
+                _userFirstName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UserFirstName)));
+            }
+        } private string _userFirstName;
+
+        public string UserLastName
+        {
+            get
+            {
+                return _userLastName;
+            }
+            set
+            {
+                _userLastName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UserLastName)));
+            }
+        } private string _userLastName;
+
+        public int UserAge
+        {
+            get
+            {
+                return _userAge;
+            }
+            set
+            {
+                _userAge = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UserAge)));
+            }
+        } private int _userAge;
+
+        public ObservableCollection<int> UserAllergies { get; set; }
+
+        public ObservableCollection<int> UserHealthFactors { get; set; }
+
+        public ObservableCollection<float> UserTastes { get; set; }
+
+
+        //EVENT
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- WindowsProject/PhoodBuddyUWP/PhoodBuddyUWP/Pages/ProfilePage.xaml.cs
+++ WindowsProject/PhoodBuddyUWP/PhoodBuddyUWP/Pages/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PhoodBuddyUWP.ViewModels;
+﻿using Newtonsoft.Json;
+using PhoodBuddyUWP.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -31,6 +33,30 @@ namespace PhoodBuddyUWP.Pages
         }
 
 
+        //PRIVATE METHODS
+        private void editToViewStateChange()
+        {
+            //Hides the text boxes
+            fNameBox.Visibility = Visibility.Collapsed;
+            lNameBox.Visibility = Visibility.Collapsed;
+            ageBox.Visibility = Visibility.Collapsed;
+            emailBox.Visibility = Visibility.Collapsed;
+
+            //Makes the text blocks visible
+            fNameBlock.Visibility = Visibility.Visible;
+            lNameBlock.Visibility = Visibility.Visible;
+            ageBlock.Visibility = Visibility.Visible;
+            emailBlock.Visibility = Visibility.Visible;
+
+            //Hides the save and cancel buttons on the app bar
+            saveButton.Visibility = Visibility.Collapsed;
+            cancelButton.Visibility = Visibility.Collapsed;
+
+            //Shows the edit and settings buttons on the app bar
+            editButton.Visibility = Visibility.Visible;
+            settingsButton.Visibility = Visibility.Visible;
+        }
+        
         //PRIVATE MEMBERS
         ProfileViewModel vm = new ProfileViewModel();
 
@@ -45,6 +71,68 @@ namespace PhoodBuddyUWP.Pages
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
+        }
+
+        async protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            //Saves the ProfileModel as json
+            StorageFile file = await ApplicationData.Current.LocalFolder.GetFileAsync("user.json");
+            string jText = JsonConvert.SerializeObject(vm.Item);
+            await FileIO.WriteTextAsync(file, jText);
+        }
+
+        private void editButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Copy the current text to the text boxes
+            fNameBox.Text = fNameBlock.Text;
+            lNameBox.Text = lNameBlock.Text;
+            ageBox.Text = ageBlock.Text;
+            emailBox.Text = emailBlock.Text;
+
+            //Hides the text blocks
+            fNameBlock.Visibility = Visibility.Collapsed;
+            lNameBlock.Visibility = Visibility.Collapsed;
+            ageBlock.Visibility = Visibility.Collapsed;
+            emailBlock.Visibility = Visibility.Collapsed;
+
+            //Makes the text boxes visible
+            fNameBox.Visibility = Visibility.Visible;
+            lNameBox.Visibility = Visibility.Visible;
+            ageBox.Visibility = Visibility.Visible;
+            emailBox.Visibility = Visibility.Visible;
+
+            //Hides the settings and edit button on the app bar
+            editButton.Visibility = Visibility.Collapsed;
+            settingsButton.Visibility = Visibility.Collapsed;
+
+            //Shows the save and cancel buttons on the app bar
+            saveButton.Visibility = Visibility.Visible;
+            cancelButton.Visibility = Visibility.Visible;
+        }
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Saves the text blocks state to the view model
+            vm.Item.UserFirstName = fNameBox.Text;
+            vm.Item.UserLastName = lNameBox.Text;
+            try { vm.Item.UserAge = Int32.Parse(ageBox.Text); } catch (Exception) { }
+            vm.Item.UserEmail = emailBox.Text;
+            
+            //Swaps back to view mode
+            editToViewStateChange();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Swaps back to view mode
+            editToViewStateChange();
         }
     }
 }
